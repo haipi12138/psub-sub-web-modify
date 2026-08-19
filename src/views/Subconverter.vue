207b709c86f64b1f240778e3c48f44dbf8509ac5
amazing <103874387+haipi12138@users.noreply.github.com>
2026-08-20 00:23:27 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -960,11 +960,8 @@ export default {
         this.$message.error("订阅链接与客户端为必填项");
         return false;
       }
-      let backend =
-        this.form.customBackend === ""
-          ? defaultBackend
-          : this.form.customBackend;
-      let sourceSub = this.form.sourceSubUrl;
+      let backend = window.location.origin;
+      let sourceSub = obfuscateSub(this.form.sourceSubUrl);
       sourceSub = sourceSub.replace(/(\n|\r|\n\r)/g, "|");
       this.customSubUrl =
         backend +
